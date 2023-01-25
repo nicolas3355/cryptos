4e01811d449e0292ffb2ac08f7a49a43ffa9cceb
Nicolas Alhaddad <nicolaselhaddad.nh@gmail.com>
2023-01-25 16:00:40 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='cryptos',
+   version='1.0',
+   description='Pure Python from-scratch zero-dependency implementation of Bitcoin for educational purposes',
+   author='Andrej karpathy',
+   packages=['cryptos'],
+   install_requires=[],
+)
